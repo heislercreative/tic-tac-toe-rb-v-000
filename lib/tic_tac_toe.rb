31eeac3fc8d72c15c2b45c59ffd8ef4308ae598d
Brennan Heisler <learn@heislercreative.com>
2018-01-14 00:22:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,7 +95,7 @@ def play(board)
   until over?(board)
     turn(board)
   end
-  if over?(board) && won?(board)
+  if won?(board)
     puts "Congratulations #{board[winning_token[0]]}!"
   elsif draw?(board)
     puts "Cat's Game!"
